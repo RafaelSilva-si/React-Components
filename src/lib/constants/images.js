c3edd02a028218f4bf53ffb30f531d170bcb826a
rafaelsilva-si <rafaelkaperski@gmail.com>
2022-03-28 12:23:35 -0300

    Base for project

---
+++ src/lib/constants/images.js
@@ -0,0 +1,9 @@
+/* eslint-disable global-require */
+const images = {
+	
+    logo_teste: require('../../assets/img/users/modelo.jpeg'),
+    logo_oficial: require('../../assets/img/logo/React.png')
+
+};
+
+export default images;
